831d5f4eaf493b6fa075dba626950e6020ae0f18
JosseG <74014165+JosseG@users.noreply.github.com>
2023-05-18 12:57:28 -0500

    Update CitaCDetalles.cs

--- bot/MyBotConversational/MyBotConversational/CitaCDetalles.cs
+++ bot/MyBotConversational/MyBotConversational/CitaCDetalles.cs
@@ -1,12 +1,8 @@
-﻿namespace MyBotConversational
+namespace MyBotConversational
 {
     public class CitaCDetalles
     {
-        public string animal { get; set; }
-
-        public string nombre { get; set; }
-
-        public string tipocita { get; set; }
+        public string idcita { get; set; }
 
     }
 }
